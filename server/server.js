1534838f77c7e7f03ae553d5e36e89fc8a2be3a6
iamasianman <kevinle1991@gmail.com>
2021-04-26 21:37:37 -0700

    implemented kev doc and code clean up

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Document = require('./Document')
+const Document = require('./Document');
 
 mongoose.connect('mongodb://localhost/kevDoc', {
   useNewUrlParser: true,
@@ -15,26 +15,27 @@ const io = require('socket.io')(3001, {
   },
 })
 
-const defaultValue = ''
+const defaultValue = '';
 
 io.on('connection', socket => {
   socket.on('get-document', async documentId => {
-    const document = await findOrCreateDocument(documentId)
-    socket.join(documentId)
-    socket.emit('load-document', document.data)
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit('load-document', document.data);
     socket.on('send-changes', delta => {
-      socket.broadcast.emit('receive-changes', delta)
+      socket.broadcast.to(documentId).emit('receive-changes', delta);
 
       socket.on('save-document', async data => {
-        await Document.findByIdAndUpdate(documentId, { data })
+        await Document.findByIdAndUpdate(documentId, { data });
       })
     })
   })
 })
 
-function findOrCreateDocument(id) {
-  if (id == null) return
-  const document = await Document.findById(id)
-  if (document) return document
-  return await Document.create({ _id: id, data: defaultValue })
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
 }
